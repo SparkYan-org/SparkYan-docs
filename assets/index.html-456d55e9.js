e8e77d1ced4b66f965b1e17ffa58f3ea696f0697
moyanj <1561515308@qq.com>
2024-08-05 18:27:58 +0000

    Deploying to gh-pages from @ SparkYan-org/SparkYan-docs@a62945cd308fabe449cf0139a4b5abad02141ad1 🚀

---
+++ assets/index.html-456d55e9.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as s,a as o}from"./app-4e7417d3.js";const n={};function p(i,a){return t(),s("div",null,[o(` ## 平台
+
+我目前使用了以下软件厂商提供的服务
+
+<a href="https://github.com">
+    <img src="/assets/icon/github_logo.jpg" alt="Deploys by Github" weight="100px"/>
+</a> &nbsp;
+<a href="https://pypi.org"><img src="/assets/icon/pypi_logo.jpg" alt="Deploys by Pypi" ></a> &nbsp;
+<a href="https://reqlit.com"><img src="/assets/icon/replit_logo.jpg" alt="Deploys by Replit" weight="100px"/></a> &nbsp; `)])}const l=e(n,[["render",p],["__file","index.html.vue"]]);export{l as default};
